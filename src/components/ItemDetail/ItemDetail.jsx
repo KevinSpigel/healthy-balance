1d7e81f5bce08f26ed5f695d7f1cd6a4e3e8cd96
KevinSpigel <kevinspigel@gmail.com>
2022-10-09 16:35:12 +0200

    Detail cards were developed

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -13,36 +13,33 @@ export const ItemDetail = ({ item }) => {
 
     return (
         <div>
-            <div className="">
-            <Card className="cardDiv">
-                <Card.Img src={item.imgProduct} alt={item.name} />
-                <Card.Body >
-                    <Card.Title>{item.name} - ${item.price}</Card.Title>
-                    <Link to={`/item/${item.id}`}><Button variant="primary">Ver detalle</Button></Link>
-                </Card.Body>
-            </Card>
+            <div className="detailDiv">
 
-            <Card className="cardDiv">
-                <Card.Img src={item.imgProduct} alt={item.name} />
-                <Card.Body >
-                    <Card.Title>{item.name} - ${item.price}</Card.Title>
-                    <Link to={`/item/${item.id}`}><Button variant="primary">Ver detalle</Button></Link>
-                </Card.Body>
-            </Card>
+                <Card className="cardDiv">
+
+                    <Card.Body >
+                        <Card.Title>Descripción del producto</Card.Title>
+
+                        <Card.Text>
+                            {item.description}
+                        </Card.Text>
+
+
+                    </Card.Body>
+                </Card>
+
+                <Card className="cardDiv">
+                    <Card.Img src={item.imgProduct} alt={item.name} />
+                    <Card.Body >
+                        <Card.Title>{item.name} - ${item.price}</Card.Title>
+                        <Card.Text>
+                            <ItemCount />
+
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
             </div>
         </div>
 
     )
 };
-
-
-{/* <h4>{item.name}</h4>
-
-<div>
-    <img src={item.imgProduct} alt={item.name} />
-</div>
-
-<div>
-
-    <h5>${item.price}</h5>
-</div> */}
