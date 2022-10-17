dc7839e260a3a73ea1cdba892bc8ed1d5b8c8d2c
KevinSpigel <kevinspigel@gmail.com>
2022-10-17 23:40:00 +0200

    Worked on Cart component styles

--- src/components/ItemCart/ItemCart.jsx
+++ src/components/ItemCart/ItemCart.jsx
@@ -1,10 +1,14 @@
 import '../ItemCart/ItemCartStyles/ItemCartStyles.css';
+import Card from 'react-bootstrap/Card';
 
 
-export const ItemCart=()=>{
-    return(
-        <div>
-            
+export const ItemCart = () => {
+    return (
+        <div className="itemCartContainerDiv">
+
+            <h2>Tu Carrito de compras</h2>
+
+
         </div>
     )
 }
