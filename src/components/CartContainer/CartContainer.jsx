0b5afde23751ac5fd82182006e86381f17c3eb50
KevinSpigel <kevinspigel@gmail.com>
2022-10-23 14:44:13 +0200

    Worked on CartContainer styles

--- src/components/CartContainer/CartContainer.jsx
+++ src/components/CartContainer/CartContainer.jsx
@@ -1,8 +1,12 @@
 import './CartContainerStyles/CartContainerStyles.css';
 
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
 import { useContext } from 'react';
 import { CartContext } from '../../context/CartContext';
-
 
 
 export const CartContainer = () => {
@@ -12,21 +16,34 @@ export const CartContainer = () => {
 
     return (
         <div>
-            <div style={{ width: "500px" }}>
+
+            <div className="" style={{ width: "500px" }}>
                 {
                     cartProducts.map((product) => (
-                        <div>
-                            <p>{product.name}</p>
-                            <p>Precio unitario{product.price}</p>
-                            <p>Cantidad{product.quantity}</p>
-                            <p>Precio por cantidad{product.quantityPrice}</p>
-                            <button onClick={() => removeItem(product.id)}>Eliminar producto</button>
+                        <div >
+                            {/* <p>{product.name}</p>
+                            <p>Precio unitario: ${product.price}</p>
+                            <p>Cantidad: {product.quantity}</p>
+                            <p>Precio por cantidad: ${product.quantityPrice}</p>
+                            <Button className="removeProductButton" onClick={() => removeItem(product.id)}><FontAwesomeIcon className="fa-light" icon={faTrash}></FontAwesomeIcon></Button> */}
+
+                            <Card className="m-4">
+                                <Card.Body className="cartProductDiv" >
+                                    <Card.Text><img className="imgCart" src={product.imgProduct} alt={product.name}></img></Card.Text>
+                                    <Card.Text><div>{product.name}</div></Card.Text>
+                                    <Card.Text><div>${product.price}</div></Card.Text>
+                                    <Card.Text><div>${product.quantityPrice}</div></Card.Text>
+                                    <Card.Text> <Button className="removeProductButton" onClick={() => removeItem(product.id)}><FontAwesomeIcon className="fa-light" icon={faTrash}></FontAwesomeIcon></Button>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
                         </div>
                     ))
-
                 }
-                <p>Precio total: {getTotalPrice()} </p>
             </div>
+
+            <Card className="totalPrice h4">Precio total: ${getTotalPrice()} </Card>
+
         </div>
     )
 }
