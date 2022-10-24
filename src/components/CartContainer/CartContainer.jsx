87e968a9cdb043b15c39b8cb37d6e58bf2558063
KevinSpigel <kevinspigel@gmail.com>
2022-10-25 01:14:50 +0200

    Worked on empty cart styles

--- src/components/CartContainer/CartContainer.jsx
+++ src/components/CartContainer/CartContainer.jsx
@@ -17,18 +17,25 @@ export const CartContainer = () => {
     const { cartProducts, getTotalPrice, getTotalProducts, removeItem, addProductCart, deleteProductCart, emptyCart } = value;
 
 
-    if(cartProducts.length===0){
+    if (cartProducts.length === 0) {
         return (
-            <div>
-            <div> El carrito esta vacio.</div>
-            <Link to="/"><Button>Ir a los productos</Button></Link>
-            </div>
+            <div className="cartContainer">
+                
+                    <Card className="cardCartContainer">
+                        <Card.Text>
+                            <h1>Lo siento,</h1>
+                            <h3>Aún no tienes productos en tu carrito</h3>
+                            <Link to="/"><Button className="btn-lg mt-3 mb-3 counterButton">Ver todos los productos</Button></Link>
+                        </Card.Text>
+                    </Card>
+                </div>
+            
         )
     }
 
 
     return (
-        <div>
+        <div className="cartContainer">
 
             <div style={{ width: "650px" }}>
                 {
@@ -39,10 +46,14 @@ export const CartContainer = () => {
                                     <Card.Text><img className="imgCart" src={product.imgProduct} alt={product.name}></img></Card.Text>
                                     <Card.Text><div>{product.name}</div></Card.Text>
                                     <Card.Text><div>${product.price}</div></Card.Text>
-                                    <Card.Text><div>{product.quantity}</div></Card.Text>
+
+                                    <div className="counterCart">
+                                        <Card.Text> <Button className="btn btn-sm btn-secondary" onClick={() => deleteProductCart(product.id)}>-</Button></Card.Text>
+                                        <Card.Text>{product.quantity}</Card.Text>
+                                        <Card.Text> <Button className="btn btn-sm btn-secondary" onClick={() => addProductCart(product.id)}>+</Button></Card.Text>
+                                    </div>
+
                                     <Card.Text><div>${product.quantityPrice}</div></Card.Text>
-                                    <Card.Text> <Button onClick={() => addProductCart(product.id)}>+</Button></Card.Text>
-                                    <Card.Text> <Button onClick={() => deleteProductCart(product.id)}>-</Button></Card.Text>
                                     <Card.Text> <Button className="removeProductButton" onClick={() => removeItem(product.id)}><FontAwesomeIcon className="fa-light" icon={faTrash}></FontAwesomeIcon></Button>
                                     </Card.Text>
                                 </Card.Body>
@@ -52,10 +63,15 @@ export const CartContainer = () => {
                 }
             </div>
 
-            <Card className="totalPrice h3">Precio total: ${getTotalPrice()} </Card>
-            <Card className="totalPrice h3">Cantidad total: {getTotalProducts()} </Card>
-            <Button onClick={emptyCart}>Borrar todo</Button>
+            <div className="generalCards">
+                <div>
+                    <Card className="totalPrice h5">Precio total: ${getTotalPrice()} </Card>
+                    <Card className="totalPrice h5">Cantidad total: {getTotalProducts()} </Card>
+                    <Button className="btn btn-lg btn-success">Terminar compra</Button>
 
+                </div>
+                <Button className="btn btn-sm btn-danger" onClick={emptyCart}>Vaciar carrito</Button>
+            </div>
 
         </div>
     )
